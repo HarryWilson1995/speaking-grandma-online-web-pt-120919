4554d6de1ea7748e84256a7859372e4e69320348
Harry Wilson <harry.wilson1995a@gmail.com>
2019-12-10 00:42:24 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
-  if string = "I LOVE YOU GRANDMA"
+  if string == "I LOVE YOU GRANDMA"
     p "I LOVE YOU TOO PUMPKIN!"
   elsif string == string.upcase 
     p "NO, NOT SINCE 1938!"
